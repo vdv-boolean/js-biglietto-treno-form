004ac6e9a5d079efb5523e311b82e07c29dd206b
vincenzodivita <divitavincenzo@icloud.com>
2023-04-13 17:30:51 +0200

    Add ";" text on JS innerHTML

--- assets/script.js
+++ assets/script.js
@@ -22,32 +22,32 @@ let cabinShow = document.querySelector('#cabinShow')
 const btn = document.querySelector("button"); 
 btn.addEventListener('click', function () {
     
-    passengerNameShow.innerHTML = passengerName.value;
+    passengerNameShow.innerHTML = passengerName.value + ";";
     let ticketSale = 0;
     let ticketPriceFinal = 0;
     ticketPrice = journeyDistance.value * ratePerKM;
     if (passengerAge.value < 18) {
         ticketSale = ticketPrice / 5;
         ticketPriceFinal = ticketPrice - ticketSale;
-        ticketPriceShow.innerHTML = ticketPriceFinal + " €";
-        saleShow.innerHTML = "Sconto minorenni";
+        ticketPriceShow.innerHTML = ticketPriceFinal + " €;";
+        saleShow.innerHTML = "Sconto minorenni;";
     }
     
     else if (passengerAge.value >= 65) {
         ticketSale = ticketPrice / 2.5;
         ticketPriceFinal = ticketPrice - ticketSale;
-        ticketPriceShow.innerHTML = ticketPriceFinal + " €";
-        saleShow.innerHTML = "Sconto Over 65";
+        ticketPriceShow.innerHTML = ticketPriceFinal + " €;";
+        saleShow.innerHTML = "Sconto Over 65;";
     }
     
     else  {
         ticketPriceFinal = ticketPrice - ticketSale;
-        ticketPriceShow.innerHTML = ticketPriceFinal + " €";
-        saleShow.innerHTML = "Tariffa base";
+        ticketPriceShow.innerHTML = ticketPriceFinal + " €;";
+        saleShow.innerHTML = "Tariffa base;";
     }
 
-    cabinShow.innerHTML = (Math.floor(Math.random()* 9) + 1);
-    codeShow.innerHTML = (Math.floor(Math.random()* (99999 - 10000) + 10000));  
+    cabinShow.innerHTML = (Math.floor(Math.random()* 9) + 1)  + ";";
+    codeShow.innerHTML = (Math.floor(Math.random()* (99999 - 10000) + 10000))  + ";";  
 
 });
 
